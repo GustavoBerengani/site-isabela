80bdcd072c52f67f3b8ef8b1eeec0f5cfccdde28
GustavoBerengani <164219563+GustavoBerengani@users.noreply.github.com>
2026-09-24 13:29:24 -0300

    Remove frase do rodape e configura video do Sim

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,5 @@
 // Cole aqui o link do vídeo quando estiver pronto (YouTube ou youtu.be).
 // Enquanto estiver vazio, o botão Sim mostra a mensagem de carinho.
 window.ISABELA_CONFIG = Object.freeze({
-  youtubeUrl: "",
+  youtubeUrl: "https://youtu.be/ybpRjw6XJsY?si=pVf-VvuXkmxDcOpw",
 });
